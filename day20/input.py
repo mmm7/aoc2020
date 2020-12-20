e3de6f793bb35f8514c40c974632dae9e2fff12f
Mate <mate@noreply.com>
2020-12-20 13:30:14 +0100

    input cleanup

--- day20/input.py
+++ day20/input.py
@@ -1,15 +1,20 @@
 import numpy as np
 import sys
 
+# { tile: array }
+# array is 2D numpy array with 0 and 1.
 T={}
+
+# { tile: (edge, edge, edge, edge) }
+# Negative tile means flipped.
 E={}
 
 lines = [x.strip() for x in sys.stdin]
 
-def _dec(c):
+def _decode(c):
   return (0,1)[c=='#']
 
-def _vectors(m):
+def _edges(m):
   for _ in range(4):
     yield int(''.join(map(str,m[:1, 0:][0])), 2)
     m = np.rot90(m)
@@ -25,13 +30,11 @@ while True:
   img = []
   while lines:
     l = lines.pop(0)
-    #print(l)
     if not l: break
-    img.append(list(map(_dec,l)))
+    img.append(list(map(_decode,l)))
   imm = np.array(img)
-  #print(img,'\n',imm)
   T[tile] = imm
-  E[tile] = list(_vectors(imm))
+  E[tile] = list(_edges(imm))
   imm=np.transpose(imm)
   T[-tile] = imm
-  E[-tile] = list(_vectors(imm))
+  E[-tile] = list(_edges(imm))
